bfc7bed1025bd23cce3eb51cba99d6785487a1e7
chenhuaiyu <215249714@qq.com>
2020-03-06 18:01:18 +0800

    新建kotlin项目

---
+++ Kotlin-AdapterDemo-master/app/src/main/java/chy/adapterdemo/SimpleCursorAdapterActivity.kt
@@ -0,0 +1,14 @@
+package chy.adapterdemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class SimpleCursorAdapterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simple_cursor_adapter)
+        //暂时没弄数据库
+    }
+}
